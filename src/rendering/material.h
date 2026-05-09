19417444c413477b9569230ea7b1a4992ed1b092
Aether <aether@aether-ai.online>
2026-05-09 16:12:31 -0400

    Add material system: diffuse, specular, textures

--- src/rendering/material.h
+++ src/rendering/material.h
@@ -1,13 +1,15 @@
 #pragma once
 #include "texture.h"
-#include "shader.h"
-#include <memory>
+#include "math/vec3.h"
 
-namespace VoxelEngine {
-    struct Material {
-        std::shared_ptr<Shader> shader;
-        std::shared_ptr<Texture> diffuse;
-        std::shared_ptr<Texture> normal;
-        float shininess = 32.0f;
-    };
+namespace vge {
+struct Material {
+    Vec3 diffuseColor;
+    Vec3 specularColor;
+    float shininess;
+    Texture* diffuseTexture;
+    
+    Material() : diffuseColor(1.0f, 1.0f, 1.0f), specularColor(1.0f, 1.0f, 1.0f), 
+                 shininess(32.0f), diffuseTexture(nullptr) {}
+};
 }
